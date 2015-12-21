1cc3796b3bb8fbc2ed52a5cdd84221df15ad13c7
Karin Touchie <ktouchie@gmail.com>
2015-12-21 11:52:51 +0100

    Move files from other directory

---
+++ bubble.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class bubble {
+
+	public static void main(String[] args) {
+        
+        int[] tab = {5,9,4,1,2,7,3,8,6,0};
+        
+        boolean swap = false;
+        
+        do {
+            swap = false;
+            for (int i= 0; i<tab.length-1; i++) {
+                if (tab[i]>tab[i+1]) {
+                    int x = tab[i];
+                    tab[i] = tab[i+1];
+                    tab[i+1] = x;
+                    swap = true;
+                }
+            }
+        } while (swap == true);
+        System.out.println(Arrays.toString(tab));
+    }
+}
